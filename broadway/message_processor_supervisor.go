a6f4829599d349e310662d0e8476a16520540c76
ttrung2409 <ttrung2409@gmail.com>
2025-06-21 13:03:49 +0700

    recover from message processor paniking

--- broadway/message_processor_supervisor.go
+++ broadway/message_processor_supervisor.go
@@ -1,18 +1,25 @@
 package broadway
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // messageProcessorSupervisor manages a pool of message processors, handling their
 // lifecycle and coordination with producers and batchers.
 type messageProcessorSupervisor struct {
-	config MessageProcessorConfig
-	hr     *hashRing[*messageProcessor]
+	config                    MessageProcessorConfig
+	hr                        *hashRing[*messageProcessor]
+	mu                        sync.Mutex
+	onAllProcessorsTerminated chan bool
 }
 
 func newMessageProcessorSupervisor(config MessageProcessorConfig) *messageProcessorSupervisor {
 	return &messageProcessorSupervisor{
-		config: config,
-		hr:     newHashRing[*messageProcessor](),
+		config:                    config,
+		hr:                        newHashRing[*messageProcessor](),
+		mu:                        sync.Mutex{},
+		onAllProcessorsTerminated: make(chan bool),
 	}
 }
 
@@ -30,20 +37,63 @@ func (s *messageProcessorSupervisor) Run(
 	batchers map[string]*batcher,
 ) {
 	for i := 0; i < s.config.Concurrency; i++ {
-		messageProcessor := newMessageProcessor(s.config, producers, batchers)
-		messageProcessor.Run(ctx)
+		mp := newMessageProcessor(s.config, producers, batchers)
+		mp.Run(ctx)
 
-		s.hr.AddNode(messageProcessor)
+		go func(mp *messageProcessor) {
+			if panic, ok := <-mp.OnTerminated(); ok {
+				if panic != nil {
+					s.handleProcessorPanic(mp, producers, batchers, ctx)
+				} else {
+					s.checkIfAllProcessorsTerminated()
+				}
+			}
+
+		}(mp)
+
+		s.hr.AddNode(mp)
 	}
 }
 
-// Terminate stops all message processors managed by this supervisor and releases all resources.
+// Terminate stops all message processors managed by this supervisor.
 // This should be called when shutting down the pipeline to ensure proper cleanup.
 func (s *messageProcessorSupervisor) Terminate() {
 	processors := s.hr.GetAllNodes()
 	for _, p := range processors {
 		p.Terminate()
 	}
+}
+
+func (s *messageProcessorSupervisor) handleProcessorPanic(processor *messageProcessor, producers map[string]*producer, batchers map[string]*batcher, ctx context.Context) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.hr.RemoveNode(processor)
+
+	newProcessor := newMessageProcessor(s.config, producers, batchers)
+	newProcessor.Run(ctx)
+
+	keySharingProcessor, _ := s.hr.GetNextNode(newProcessor)
+
+	if keySharingProcessor != nil {
+		keySharingProcessor.Pause()
+	}
+
+	s.hr.AddNode(newProcessor)
+
+	if keySharingProcessor != nil {
+		keySharingProcessor.Resume()
+	}
+
+	go func(p *messageProcessor) {
+		if panic, ok := <-p.OnTerminated(); ok {
+			if panic != nil {
+				s.handleProcessorPanic(p, producers, batchers, ctx)
+			} else {
+				s.checkIfAllProcessorsTerminated()
+			}
+		}
+	}(newProcessor)
 }
 
 // SetProducers updates the producers for all message processors managed by this supervisor.
@@ -90,4 +140,33 @@ func (s *messageProcessorSupervisor) Resolve(partitionKey string) (string, bool)
 	}
 
 	return "", false
+}
+
+func (s *messageProcessorSupervisor) OnAllProcessorsTerminated() <-chan bool {
+	return s.onAllProcessorsTerminated
+}
+
+func (s *messageProcessorSupervisor) checkIfAllProcessorsTerminated() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.onAllProcessorsTerminated == nil {
+		return
+	}
+
+	processors := s.hr.GetAllNodes()
+
+	allTerminated := true
+
+	for _, processor := range processors {
+		if !processor.IsTerminated() {
+			allTerminated = false
+		}
+	}
+
+	if allTerminated {
+		s.onAllProcessorsTerminated <- true
+		close(s.onAllProcessorsTerminated)
+		s.onAllProcessorsTerminated = nil
+	}
 }
